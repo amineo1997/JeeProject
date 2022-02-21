37b851a2abf36da971ea86e74f6f38037408240b
amineo1997 <amine.ouazir97@gmail.com>
2022-02-21 01:08:03 +0000

    My first commit

---
+++ customer-service/src/test/java/org/sid/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.customerservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
